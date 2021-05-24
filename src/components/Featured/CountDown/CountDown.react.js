f2b48daca792210778aae9dd29d200bb90d04618
Xiaomei Wu <xiaomeiwu@outlook.de>
2021-05-24 02:02:36 +0200

    Add countdown

--- src/components/Featured/CountDown/CountDown.react.js
+++ src/components/Featured/CountDown/CountDown.react.js
@@ -1,5 +1,64 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import { Slide } from "react-awesome-reveal";
+
+import {
+  CountDownWrapper,
+  CountDownTop,
+  CountDownBottom,
+  CountDownItem,
+  CountDownTime,
+  CountDownTag,
+} from "./CountDown.styled";
 
 export const CountDown = () => {
-  return <div>CountDown</div>;
+  const [time, setTime] = useState({
+    days: "0",
+    hours: "0",
+    minutes: "0",
+    seconds: "0",
+  });
+
+  const renderItem = (time, value) => (
+    <CountDownItem>
+      <CountDownTime>{time}</CountDownTime>
+      <CountDownTag>{value}</CountDownTag>
+    </CountDownItem>
+  );
+
+  const getTimeUntil = useCallback((deadline) => {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    if (time < 0) {
+      console.log("Date passed");
+    } else {
+      const seconds = Math.floor((time / 1000) % 60);
+      const minutes = Math.floor((time / 1000 / 60) % 60);
+      const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+      setTime({
+        days,
+        hours,
+        minutes,
+        seconds,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    setInterval(() => getTimeUntil("Dec, 20, 2021, 01:20:00"), 1000);
+  }, [getTimeUntil]);
+
+  return (
+    <Slide left delay={1000}>
+      <CountDownWrapper>
+        <CountDownTop>Event starts in</CountDownTop>
+
+        <CountDownBottom>
+          {renderItem(time.days, "Days")}
+          {renderItem(time.hours, "HS")}
+          {renderItem(time.minutes, "Min")}
+          {renderItem(time.seconds, "Sec")}
+        </CountDownBottom>
+      </CountDownWrapper>
+    </Slide>
+  );
 };
---
+++ src/components/Featured/CountDown/CountDown.styled.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const CountDownWrapper = styled.div`
+  position: absolute;
+  bottom: 0px;
+  color: #ffffff;
+`;
+export const CountDownTop = styled.div`
+  background: #ff4800;
+  font-size: 19px;
+  padding: 10px;
+  display: inline-block;
+  text-transform: uppercase;
+`;
+
+export const CountDownBottom = styled.div`
+  display: flex;
+  background: #d93d00;
+  padding: 10px 20px;
+`;
+
+export const CountDownTime = styled.div`
+  font-size: 70px;
+  border-right: 1px solid #f44336;
+  margin-right: 14px;
+  padding-right: 49px;
+`;
+
+export const CountDownItem = styled.div`
+  :last-child {
+    border: none;
+  }
+`;
+
+export const CountDownTag = styled.div`
+  text-transform: uppercase;
+  font-size: 20px;
+  padding-left: 7px;
+`;
